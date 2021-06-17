b84b04310cfe4831a6a7c521f26bebab06f00967
user <katatumura615@gmail.com>
2021-06-17 10:46:55 +0900

    Initial Commit

---
+++ aaa/aaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  aaaApp.swift
+//  aaa
+//
+//  Created by user on 2021/06/17.
+//
+
+import SwiftUI
+
+@main
+struct aaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
